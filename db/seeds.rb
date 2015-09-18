1b0edc537ed938f24321f6c938ea41748511b815
Ryan Poplawski <claydewd@gmail.com>
2015-09-17 19:10:55 -0500

    cleaned up seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,29 @@
-50.times do User.create({first_name: Faker::Name.first_name,
-  last_name: Faker::Name.last_name,
-  age: Faker::Number.between(23, 97)})
+50.times do
+  user = User.create({
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    age: Faker::Number.between(23, 97)
+    })
+  Address.create({
+    user_id: user.id,
+    street_name: Faker::Address.street_name,
+    city: Faker::Address.city,
+    state: Faker::Address.state,
+    zip: Faker::Address.zip
+    })
+end
+
+50.times do
+  Item.create({name: Faker::Commerce.product_name,
+  price: Faker::Number.between(1, 1000),
+  description: Faker::Commerce.color})
+end
+
+50.times do
+  Order.create({item_id: Faker::Number.between(1, 50),
+  user_id: Faker::Number.between(1, 50),
+  quantity: Faker::Number.between(1, 20)})
+end
 
 
 
